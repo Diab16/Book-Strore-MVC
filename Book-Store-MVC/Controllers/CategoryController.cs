658b45016945fee9caf065108fd693b3d38fd8ba
Alaa-sleem-Abdelsalam <alaa.sleem.abdelsalam@vultara.com>
2024-08-12 00:54:02 +0300

    Created Book And Category controllers made layout and login and register view

---
+++ Book-Store-MVC/Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using Book_Store_MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_Store_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly BookStoreContext bookStore;
+        private readonly IMapper mapper;
+
+        public CategoryController(BookStoreContext bookStore, IMapper mapper)
+        {
+            this.bookStore = bookStore;
+            this.mapper = mapper;
+        }
+
+
+        // GET: CategoryController
+        public ActionResult Index()
+        {
+            List<Category> categories = bookStore.Category.ToList();
+            return View(categories);
+        }
+
+       
+
+       
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+      
+        public ActionResult Create(Category category)
+        {
+             if(ModelState.IsValid)
+            {
+                bookStore.Category.Add(category);
+                bookStore.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+
+
+        }
+
+      
+        public ActionResult Edit(int id)
+        {
+            var category = bookStore.Category.Find(id);
+            return View(category);
+        }
+
+     
+        [HttpPost]
+        public ActionResult Edit( Category category)
+        {
+              if(ModelState.IsValid)
+              {
+                Category categorydb = bookStore.Category.Find(category.Id);
+                categorydb.Name = category.Name;
+                bookStore.Update(categorydb);
+                bookStore.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var category = bookStore.Category.Find(id);
+            return View(category);
+        }
+
+        [HttpPost]
+       
+        public ActionResult Delete(Category category)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var categorydb = bookStore.Category.Find(category.Id);
+                bookStore.Category.Remove(categorydb);
+                bookStore.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+    }
+}
--- Book-Store-MVC/ViewModels/BookViewModel.cs
+++ Book-Store-MVC/ViewModels/BookViewModel.cs
@@ -1,20 +1,28 @@
-﻿using Book_Store_MVC.Validation;
+﻿using Book_Store_MVC.Models;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Book_Store_MVC.Models
+namespace Book_Store_MVC.ViewModels
 {
-    public class Book
+    public class BookViewModel
     {
+
         public int Id { get; set; }
-        [Unique]
+      
         public string Title { get; set; }
+        [Required ]
         public string Description { get; set; }
+        [Required]
         public string Language { get; set; }
+        [Required]
         public DateTime DatePublished { get; set; }
-        public float Price { get; set; }
-        public string? AuthorName { get; set; }
+        [Required]
+        public float? Price { get; set; }
         public string? ImageUrl { get; set; }
-        public string? PublisherName { get; set; }
+        public IFormFile Imagefile { get; set; }
+        public string PublisherName { get; set; }
+
+        public string AuthorName { get; set; }
 
         [ForeignKey("Publisher")]
         public int PublisherId { get; set; }
@@ -24,6 +32,12 @@ namespace Book_Store_MVC.Models
 
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
+
+        public List<Category>? Categorylist { get; set; }
+        public List<Author>?Authorlist { get; set; }
+        public List<Publisher>? publisherlist { get; set; }
+
+
         public Category? Category { get; set; }
 
         [ForeignKey("Author")]
