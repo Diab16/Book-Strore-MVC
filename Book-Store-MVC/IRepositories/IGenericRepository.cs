090648fbf8d65cd8c95e04a5abb6dc4d9dcbdca8
Diab16 <abdohdiab166@gmail.com>
2024-08-12 01:57:14 +0300

    build: create generic Irepositories and repositories

---
+++ Book-Store-MVC/IRepositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Book_Store_MVC.IRepositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T item);
+        void Update(T item);
+        void Delete(T item);
+        void Save();
+    }
+}
