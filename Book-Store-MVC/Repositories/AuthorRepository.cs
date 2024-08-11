a3863a47f87cab5c99d83665096397b49cd8bee7
Diab16 <abdohdiab166@gmail.com>
2024-08-12 02:48:38 +0300

    build: create author Irepository and repository

---
+++ Book-Store-MVC/Repositories/AuthorRepository.cs
@@ -0,0 +1,19 @@
+﻿using Book_Store_MVC.IRepositories;
+using Book_Store_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_MVC.Repositories
+{
+    public class AuthorRepository : GenericRepository<Author>, IAuthorRepository
+    {
+        private BookStoreContext context;
+        private DbSet<Author> authores;
+        public AuthorRepository(BookStoreContext _context) : base(_context)
+        {
+            context = _context;
+            authores = context.Set<Author>();
+        }
+    }
+    
+}
+
