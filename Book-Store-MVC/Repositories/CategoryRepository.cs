d2ba567a6ee7a152b41141dc24fb2fed8b747a7c
Diab16 <abdohdiab166@gmail.com>
2024-08-12 02:50:27 +0300

    build: create Category Irepository and repository

---
+++ Book-Store-MVC/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Book_Store_MVC.IRepositories;
+using Book_Store_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_MVC.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private BookStoreContext context;
+        private DbSet<Category> categories;
+        public CategoryRepository(BookStoreContext _context) : base(_context)
+        {
+            context = _context;
+            categories= context.Set<Category>();
+        }
+    }
+}
