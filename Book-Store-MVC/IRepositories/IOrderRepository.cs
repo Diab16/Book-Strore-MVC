bf9677bf7caf023e1e157f0bdfd71145ec6f9695
Diab16 <abdohdiab166@gmail.com>
2024-08-12 02:47:22 +0300

    build: create order Irepository and repository

---
+++ Book-Store-MVC/IRepositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Book_Store_MVC.Models;
+using Book_Store_MVC.Repositories;
+
+namespace Book_Store_MVC.IRepositories
+{
+    public interface IOrderRepository : IGenericRepository<Order>
+    {
+
+    }
+}
