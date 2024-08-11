03af989010fbc0adbade7256c27e76b5812e1139
Diab16 <abdohdiab166@gmail.com>
2024-08-12 02:49:13 +0300

    build: create book Irepository and repository

---
+++ Book-Store-MVC/Repositories/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Book_Store_MVC.IRepositories;
+using Book_Store_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_MVC.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        private BookStoreContext context;
+        private DbSet<Book> books;
+        public BookRepository(BookStoreContext _context) : base(_context)
+        {
+            context = _context;
+            books = context.Set<Book>();
+        }
+    }
+}
