975c29cbd1b54d8b0d320732878e9e25d4facd6d
Diab16 <abdohdiab166@gmail.com>
2024-08-13 02:11:22 +0300

    feat: add search and pagination in Books

--- Book-Store-MVC/IRepositories/IBookRepository.cs
+++ Book-Store-MVC/IRepositories/IBookRepository.cs
@@ -4,5 +4,6 @@ namespace Book_Store_MVC.IRepositories
 {
     public interface IBookRepository : IGenericRepository<Book>
     {
+        IEnumerable<Book> GetAll(int categoryId, string searchTerm, int pageNumber, int pageSize);
     }
 }
