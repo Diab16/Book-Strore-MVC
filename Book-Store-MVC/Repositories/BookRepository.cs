4080208fb67f826623f377e00fdd53017dceace4
Diab16 <abdohdiab166@gmail.com>
2024-08-13 23:22:20 +0300

    feat: add book count function

--- Book-Store-MVC/Repositories/BookRepository.cs
+++ Book-Store-MVC/Repositories/BookRepository.cs
@@ -15,6 +15,11 @@ namespace Book_Store_MVC.Repositories
             books = context.Set<Book>();
         }
 
+        public int Count()
+        {
+            return books.Count();
+        }
+
         public IEnumerable<Book> GetAll(int categoryId = 0, string searchTerm = "", int pageNumber = 1, int pageSize = 10)
         {
             IEnumerable<Book> result;
