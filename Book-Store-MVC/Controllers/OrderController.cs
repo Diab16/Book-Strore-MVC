a44bd85d72012c9a2abf417e32e31a54bd350c08
abdalla <abdullaismael522@gmail.com>
2024-08-16 16:13:52 +0300

    added order controller and views

---
+++ Book-Store-MVC/Controllers/OrderController.cs
@@ -0,0 +1,43 @@
+﻿using Book_Store_MVC.Models;
+using Book_Store_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Store_MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public OrderController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = _context.Orders.Include(o => o.Customer).ToList();
+            return View(orders);
+        }
+
+        public IActionResult OrderSummary(int orderId)
+        {
+            var order = _context.Orders.Include(o => o.BookOrders)
+                .ThenInclude(bo => bo.Book)
+                .FirstOrDefault(o => o.Id == orderId);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            var orderSummaryViewModel = new OrderSummaryViewModel
+            {
+                BookOrders = order.BookOrders,
+                TotalPrice = (decimal)order.BookOrders.Sum(bo => bo.Book.Price * bo.Quantity),
+                OrderDate = order.OrderDate
+            };
+
+            return View(orderSummaryViewModel);
+        }
+    }
+}
